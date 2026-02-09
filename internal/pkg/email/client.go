50e75a3fa68b64167f6708f62eb01e91ce97ee44
Cyvadra <dj@lifesdk.com>
2026-02-09 15:50:53 +0800

    feat: email code

--- internal/pkg/email/client.go
+++ internal/pkg/email/client.go
@@ -2,12 +2,17 @@ package email
 
 import (
 	"crypto/tls"
+	"fmt"
+	"net/smtp"
 
 	"gopkg.in/gomail.v2"
 )
 
 type Client struct {
-	config *Config
+	config    *Config
+	useLocal  bool   // 是否使用本地SMTP服务器
+	localHost string // 本地SMTP服务器地址
+	localPort int    // 本地SMTP服务器端口
 }
 
 type Config struct {
@@ -24,6 +29,26 @@ func NewEmail(config *Config) *Client {
 	}
 }
 
+// NewLocalEmail 创建使用本地SMTP服务器的邮件客户端
+func NewLocalEmail(fromEmail, fromName string) *Client {
+	return &Client{
+		config: &Config{
+			UserName: fromEmail,
+			FromName: fromName,
+		},
+		useLocal:  true,
+		localHost: "localhost",
+		localPort: 25,
+	}
+}
+
+// SetLocalSMTP 设置本地SMTP服务器地址和端口
+func (c *Client) SetLocalSMTP(host string, port int) {
+	c.useLocal = true
+	c.localHost = host
+	c.localPort = port
+}
+
 type Option struct {
 	To      []string // 收件人
 	Subject string   // 邮件主题
@@ -33,12 +58,79 @@ type Option struct {
 type OptionFunc func(msg *gomail.Message)
 
 func (c *Client) do(msg *gomail.Message) error {
+	// 如果使用本地SMTP服务器
+	if c.useLocal {
+		return c.sendViaLocalSMTP(msg)
+	}
+
+	// 使用外部SMTP服务器
 	dialer := gomail.NewDialer(c.config.Host, c.config.Port, c.config.UserName, c.config.Password)
 
 	// 自动开启SSL
 	dialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
 	return dialer.DialAndSend(msg)
+}
+
+// sendViaLocalSMTP 通过本地SMTP服务器发送邮件
+func (c *Client) sendViaLocalSMTP(msg *gomail.Message) error {
+	// 获取邮件内容
+	var recipients []string
+	to := msg.GetHeader("To")
+	recipients = append(recipients, to...)
+
+	// 构建邮件内容
+	var emailContent []byte
+	msg.WriteTo(&emailWriter{content: &emailContent})
+
+	// 连接到本地SMTP服务器
+	addr := fmt.Sprintf("%s:%d", c.localHost, c.localPort)
+	client, err := smtp.Dial(addr)
+	if err != nil {
+		return fmt.Errorf("无法连接到本地SMTP服务器: %w", err)
+	}
+	defer client.Close()
+
+	// 设置发件人
+	if err := client.Mail(c.config.UserName); err != nil {
+		return fmt.Errorf("设置发件人失败: %w", err)
+	}
+
+	// 设置收件人
+	for _, recipient := range recipients {
+		if err := client.Rcpt(recipient); err != nil {
+			return fmt.Errorf("设置收件人失败: %w", err)
+		}
+	}
+
+	// 发送邮件内容
+	w, err := client.Data()
+	if err != nil {
+		return fmt.Errorf("准备发送邮件数据失败: %w", err)
+	}
+
+	_, err = w.Write(emailContent)
+	if err != nil {
+		w.Close()
+		return fmt.Errorf("写入邮件数据失败: %w", err)
+	}
+
+	err = w.Close()
+	if err != nil {
+		return fmt.Errorf("关闭邮件数据流失败: %w", err)
+	}
+
+	return client.Quit()
+}
+
+// emailWriter 用于捕获gomail生成的邮件内容
+type emailWriter struct {
+	content *[]byte
+}
+
+func (w *emailWriter) Write(p []byte) (n int, err error) {
+	*w.content = append(*w.content, p...)
+	return len(p), nil
 }
 
 func (c *Client) SendMail(email *Option, opt ...OptionFunc) error {
--- internal/provider/email.go
+++ internal/provider/email.go
@@ -6,6 +6,23 @@ import (
 )
 
 func NewEmailClient(conf *config.Config) *email.Client {
+	// 如果配置为使用本地SMTP服务器
+	if conf.Email.UseLocal {
+		client := email.NewLocalEmail(conf.Email.UserName, conf.Email.FromName)
+
+		// 如果指定了自定义的本地SMTP地址和端口
+		if conf.Email.LocalHost != "" && conf.Email.LocalPort > 0 {
+			client.SetLocalSMTP(conf.Email.LocalHost, conf.Email.LocalPort)
+		} else if conf.Email.LocalHost != "" {
+			client.SetLocalSMTP(conf.Email.LocalHost, 25)
+		} else if conf.Email.LocalPort > 0 {
+			client.SetLocalSMTP("localhost", conf.Email.LocalPort)
+		}
+
+		return client
+	}
+
+	// 使用外部SMTP服务器
 	return email.NewEmail(&email.Config{
 		Host:     conf.Email.Host,
 		Port:     conf.Email.Port,
